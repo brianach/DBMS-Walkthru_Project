dcfaa26cb03319d69eb4a996c8a06fff04e90912
brianach <>
2023-05-26 13:27:06 +0000

    add category selector for tasks

--- taskmanager/templates/static/script.js
+++ taskmanager/templates/static/script.js
@@ -2,4 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(elems, {
+        format:	"dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // category selector initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
