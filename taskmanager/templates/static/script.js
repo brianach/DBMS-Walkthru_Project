2daf686a15a64f0bb3cda79ed078652637dd8358
brianach <>
2023-05-27 15:28:01 +0000

    copy all files from CI tutorial to attempt fix

--- taskmanager/templates/static/script.js
+++ taskmanager/templates/static/script.js
@@ -6,17 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // datepicker initialization
     let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
-        defaultDate: "01 January, 1999",
-        setDefaultDate: true,
         format: "dd mmmm, yyyy",
         i18n: { done: "Select" }
     });
 
-    // category selector initialization
+    // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
-
-    // collapsible initializataion
-    let collapsibles = document.querySelectorAll(".collapsible");
-    M.Collapsible.init(collapsibles);
 });
