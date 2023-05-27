588fbca81c9b4383934ec7fa85537533e02d8e5e
brianach <>
2023-05-27 19:39:15 +0000

    Fix: moved static folder up one level

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,4 +13,5 @@ document.addEventListener("DOMContentLoaded", function () {
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
 });
