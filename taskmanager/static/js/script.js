4ba96309cdc70bd4c89baffb6a992ffeccefa197
brianach <>
2023-05-28 13:51:06 +0000

    add final code

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -14,4 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
+    
 });
