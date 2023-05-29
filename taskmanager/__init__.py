6e698889f8ce3d2278b5ee82f016b58fca3243e4
brianach <>
2023-05-29 13:18:27 +0000

    remove 1 from line 16 in init.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,10 +12,10 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-    #if uri.startswith("postgres://"):
-    #    uri = uri.replace("postgres://", "postgresql://", 1)
-    #uri = "postgres://"
-    #app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://") 
+    # remove ', 1' from just inisde closing bracket
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
